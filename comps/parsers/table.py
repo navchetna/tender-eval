d3320371e8c7f4a0ccf4dc1f161d7e59c3a4e6d7
Ervin <ervincas13@gmail.com>
2026-07-21 07:44:14 +0000

    rm older method
    Signed-off-by: Ervin <ervincas13@gmail.com>

--- comps/parsers/table.py
+++
@@ -1,5 +0,0 @@
-class Table:
-    def __init__(self, markdown_content, heading, node):
-        self.markdown_content = markdown_content
-        self.heading = heading
-        self.node = node
--- comps/parsers/text.py
+++
@@ -1,4 +0,0 @@
-class Text:
-    def __init__(self, content, node):
-        self.content = content
-        self.node = node
